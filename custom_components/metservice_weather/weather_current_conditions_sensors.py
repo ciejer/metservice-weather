04d7331fa358bbb789fa8eca84e4f6d686e6f648
Chris Jenkins <chris@chrisjenkins.nz>
2024-11-06 21:52:01 +1300

    Fixed some broken mobile sensors from one of yesterday's changes

--- custom_components/metservice_weather/weather_current_conditions_sensors.py
+++ custom_components/metservice_weather/weather_current_conditions_sensors.py
@@ -31,57 +31,21 @@ from homeassistant.const import (
     UnitOfSpeed,
 )
 from homeassistant.helpers.typing import StateType
-
-import logging
-_LOGGER = logging.getLogger(__name__)
-
 AUCKLAND_TIMEZONE = dt_util.get_time_zone("Pacific/Auckland")
 
 @dataclass
 class WeatherRequiredKeysMixin:
     """Mixin for required keys."""
 
-    value_fn: Callable[[Any, str], StateType]
+    value_fn: Callable[[dict[str, Any], str], StateType]
 
 
 @dataclass
 class WeatherSensorEntityDescription(SensorEntityDescription, WeatherRequiredKeysMixin):
     """Describes MetService Sensor entity."""
 
-    attr_fn: Callable[[Any], dict[str, Any]] = lambda _: {}
+    attr_fn: Callable[[dict[str, Any]], dict[str, StateType]] = lambda _: {}
     unit_fn: Callable[[bool], str | None] = lambda _: None
-
-
-# Helper functions for truncation
-def truncate_state(data: Any) -> str:
-    """Truncate state to 252 characters and append ellipsis if necessary."""
-    if data is None:
-        return "No warnings"
-    elif isinstance(data, str) and len(data) > 255:
-        truncated = f"{data[:252]}..."
-        # _LOGGER.info(f"Truncated state: {truncated}")
-        return truncated
-    elif isinstance(data, str):
-        _LOGGER.info(f"State without truncation: {data}")
-        return data
-    else:
-        _LOGGER.info("No warnings available.")
-        return "No warnings"
-
-
-def truncate_attribute(data: Any) -> dict[str, Any]:
-    """Truncate warnings attribute to 16,380 characters and append ellipsis if necessary."""
-    if isinstance(data, str):
-        if len(data) > 16384:
-            truncated_attr = {"warnings": f"{data[:16380]}..."}
-            # _LOGGER.info(f"Truncated attribute: {truncated_attr}")
-            return truncated_attr
-        else:
-            # _LOGGER.info(f"Attribute without truncation: {data}")
-            return {"warnings": data}
-    else:
-        # _LOGGER.info("No warnings to set in attributes.")
-        return {}
 
 
 current_condition_sensor_descriptions_public = [
@@ -92,7 +56,7 @@ current_condition_sensor_descriptions_public = [
         icon="mdi:clock",
         value_fn=lambda data, _: datetime.datetime.fromisoformat(data)
         if isinstance(data, str)
-        else "Unknown",
+        else None,
     ),
     WeatherSensorEntityDescription(
         key=FIELD_DESCRIPTION,
@@ -112,19 +76,19 @@ current_condition_sensor_descriptions_public = [
         device_class=SensorDeviceClass.HUMIDITY,
         state_class=SensorStateClass.MEASUREMENT,
         unit_fn=lambda _: PERCENTAGE,
-        value_fn=lambda data, _: cast(int, data) if isinstance(data, (int | float)) else 0,
+        value_fn=lambda data, _: cast(int, data) or 0,
     ),
     WeatherSensorEntityDescription(
         key="uvIndex",
         name="UV Index",
         icon="mdi:sunglasses",
-        value_fn=lambda data, _: cast(str, data) if isinstance(data, str) else "Unknown",
+        value_fn=lambda data, _: cast(str, data.replace("status-", "") if data else None),
     ),
     WeatherSensorEntityDescription(
         key=FIELD_WINDDIR,
         name="Wind Direction",
         icon=ICON_WIND,
-        value_fn=lambda data, _: cast(str, data) if isinstance(data, str) else "Unknown",
+        value_fn=lambda data, _: cast(str, data),
     ),
     WeatherSensorEntityDescription(
         key="temperatureFeelsLike",
@@ -135,7 +99,7 @@ current_condition_sensor_descriptions_public = [
         unit_fn=lambda metric: UnitOfTemperature.CELSIUS
         if metric
         else UnitOfTemperature.FAHRENHEIT,
-        value_fn=lambda data, _: cast(float, data) if isinstance(data, (int | float)) else 0.0,
+        value_fn=lambda data, _: cast(float, data),
     ),
     WeatherSensorEntityDescription(
         key=FIELD_TEMP,
@@ -146,7 +110,7 @@ current_condition_sensor_descriptions_public = [
         unit_fn=lambda metric: UnitOfTemperature.CELSIUS
         if metric
         else UnitOfTemperature.FAHRENHEIT,
-        value_fn=lambda data, _: cast(float, data) if isinstance(data, (int | float)) else 0.0,
+        value_fn=lambda data, _: cast(float, data),
     ),
     WeatherSensorEntityDescription(
         key=FIELD_PRESSURE,
@@ -155,7 +119,7 @@ current_condition_sensor_descriptions_public = [
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.PRESSURE,
         unit_fn=lambda metric: UnitOfPressure.MBAR if metric else UnitOfPressure.INHG,
-        value_fn=lambda data, _: cast(float, data) if isinstance(data, (int | float)) else 0.0,
+        value_fn=lambda data, _: cast(float, data),
     ),
     WeatherSensorEntityDescription(
         key=FIELD_WINDGUST,
@@ -166,7 +130,7 @@ current_condition_sensor_descriptions_public = [
         unit_fn=lambda metric: UnitOfSpeed.KILOMETERS_PER_HOUR
         if metric
         else UnitOfSpeed.MILES_PER_HOUR,
-        value_fn=lambda data, _: cast(float, data) if isinstance(data, (int | float)) else 0.0,
+        value_fn=lambda data, _: cast(float, data),
     ),
     WeatherSensorEntityDescription(
         key=FIELD_WINDSPEED,
@@ -177,61 +141,60 @@ current_condition_sensor_descriptions_public = [
         unit_fn=lambda metric: UnitOfSpeed.KILOMETERS_PER_HOUR
         if metric
         else UnitOfSpeed.MILES_PER_HOUR,
-        value_fn=lambda data, _: cast(float, data) if isinstance(data, (int | float)) else 0.0,
+        value_fn=lambda data, _: cast(float, data),
     ),
     WeatherSensorEntityDescription(
         key="pressureTendencyTrend",
         name="Pressure Tendency Trend",
         icon="mdi:gauge",
-        value_fn=lambda data, _: cast(str, data) if isinstance(data, str) else "Unknown",
+        value_fn=lambda data, _: cast(str, data),
     ),
     WeatherSensorEntityDescription(
         key="pollen_levels",
         name="Pollen Levels",
         icon="mdi:flower",
-        value_fn=lambda data, _: cast(str, data) if isinstance(data, str) else "Unknown",
+        value_fn=lambda data, _: cast(str, data),
     ),
     WeatherSensorEntityDescription(
         key="pollen_type",
         name="Pollen Type",
         icon="mdi:flower",
-        value_fn=lambda data, _: (
-            ". ".join(i.capitalize() for i in data.lstrip(" ").split(". ")[:20])[:255] + ("..." if len(data) > 255 else "")
-            if isinstance(data, str)
-            else "Unknown"
-        ),
-        # Pollen Type can be very long, so truncate to 255 characters and capitalize sentences
+        value_fn=lambda data, _: cast(
+            str, ". ".join(i.capitalize() for i in data.lstrip(" ")[0:254].split(". "))
+        )
+        if data else None,
+        # Pollen Type can be very long, so truncate to 254 characters; and capitalise each sentence
     ),
     WeatherSensorEntityDescription(
         key="weather_warnings",
         name="MetService Weather Warnings",
         icon="mdi:alert",
-        value_fn=lambda data, _: truncate_state(data) if data else "No warnings",
-        attr_fn=lambda data: truncate_attribute(data),
+        value_fn=lambda data, _: (data[:250] + '...') if data and len(data) > 255 else (data or "No warnings"),
+        attr_fn=lambda data: {"warnings": data} if data else {},
     ),
     WeatherSensorEntityDescription(
         key="fire_season",
         name="Fire Season",
         icon="mdi:fire",
-        value_fn=lambda data, _: cast(str, data) if isinstance(data, str) else "Unknown",
+        value_fn=lambda data, _: cast(str, data),
     ),
     WeatherSensorEntityDescription(
         key="fire_danger",
         name="Fire Danger",
         icon="mdi:fire",
-        value_fn=lambda data, _: cast(str, data) if isinstance(data, str) else "Unknown",
+        value_fn=lambda data, _: cast(str, data),
     ),
     WeatherSensorEntityDescription(
         key="drying_index_morning",
         name="Clothes Drying Time - Morning",
         icon="mdi:tshirt-crew",
-        value_fn=lambda data, _: cast(str, data.replace("Morning: ", "")) if isinstance(data, str) else "Unknown",
+        value_fn=lambda data, _: cast(str, data.replace("Morning: ", "")) if data else None,
     ),
     WeatherSensorEntityDescription(
         key="drying_index_afternoon",
         name="Clothes Drying Time - Afternoon",
         icon="mdi:tshirt-crew",
-        value_fn=lambda data, _: cast(str, data.replace("Afternoon: ", "")) if isinstance(data, str) else "Unknown",
+        value_fn=lambda data, _: cast(str, data.replace("Afternoon: ", "")) if data else None,
     ),
     WeatherSensorEntityDescription(
         key="tides_high",
@@ -246,8 +209,12 @@ current_condition_sensor_descriptions_public = [
                 and dt_util.parse_datetime(a["time"]) is not None
                 and dt_util.parse_datetime(a["time"]) > dt_util.now(AUCKLAND_TIMEZONE)
             ][0]
-            if isinstance(data, list) and data else None
-        ),
+            if data
+            else None
+        )
+        if isinstance(data, list)
+        else None,
+
     ),
 
     WeatherSensorEntityDescription(
@@ -255,7 +222,6 @@ current_condition_sensor_descriptions_public = [
         name="Next Low Tide",
         icon="mdi:beach",
         device_class=SensorDeviceClass.TIMESTAMP,
-
         value_fn=lambda data, _: (
             [
                 dt_util.parse_datetime(a["time"])
@@ -264,8 +230,11 @@ current_condition_sensor_descriptions_public = [
                 and dt_util.parse_datetime(a["time"]) is not None
                 and dt_util.parse_datetime(a["time"]) > dt_util.now(AUCKLAND_TIMEZONE)
             ][0]
-            if isinstance(data, list) and data else None
-        ),
+            if data
+            else None
+        )
+        if isinstance(data, list)
+        else None,
     ),
 ]
 
@@ -277,7 +246,7 @@ current_condition_sensor_descriptions_mobile = [
         icon="mdi:clock",
         value_fn=lambda data, _: datetime.datetime.fromisoformat(data)
         if isinstance(data, str)
-        else "Unknown",
+        else None,
     ),
     WeatherSensorEntityDescription(
         key=FIELD_DESCRIPTION,
@@ -296,31 +265,31 @@ current_condition_sensor_descriptions_mobile = [
         device_class=SensorDeviceClass.HUMIDITY,
         state_class=SensorStateClass.MEASUREMENT,
         unit_fn=lambda _: PERCENTAGE,
-        value_fn=lambda data, _: cast(int, data) if isinstance(data, (int | float)) else 0,
+        value_fn=lambda data, _: cast(int, data) or 0,
     ),
     WeatherSensorEntityDescription( # UV index from main endpoint is UV Alert from mobile endpoint
         key="uvAlert",
         name="UV Alert",
         icon="mdi:sunglasses",
-        value_fn=lambda data, _: cast(str, data) if isinstance(data, str) else "Unknown",
+        value_fn=lambda data, _: cast(str, data),
     ),
     WeatherSensorEntityDescription(
         key=FIELD_WINDDIR,
         name="Wind Direction",
         icon=ICON_WIND,
-        value_fn=lambda data, _: cast(str, data) if isinstance(data, str) else "Unknown",
+        value_fn=lambda data, _: cast(str, data),
     ),
-    WeatherSensorEntityDescription(
-        key="temperatureFeelsLike",
-        name="Temperature - Feels Like",
-        icon=ICON_THERMOMETER,
-        state_class=SensorStateClass.MEASUREMENT,
-        device_class=SensorDeviceClass.TEMPERATURE,
-        unit_fn=lambda metric: UnitOfTemperature.CELSIUS
-        if metric
-        else UnitOfTemperature.FAHRENHEIT,
-        value_fn=lambda data, _: cast(float, data) if isinstance(data, (int | float)) else 0.0,
-    ),
+    # WeatherSensorEntityDescription(
+    #     key="temperatureFeelsLike",
+    #     name="Temperature - Feels Like",
+    #     icon=ICON_THERMOMETER,
+    #     state_class=SensorStateClass.MEASUREMENT,
+    #     device_class=SensorDeviceClass.TEMPERATURE,
+    #     unit_fn=lambda metric: UnitOfTemperature.CELSIUS
+    #     if metric
+    #     else UnitOfTemperature.FAHRENHEIT,
+    #     value_fn=lambda data, _: cast(float, data),
+    # ),
     WeatherSensorEntityDescription(
         key=FIELD_TEMP,
         name="Temperature",
@@ -330,7 +299,7 @@ current_condition_sensor_descriptions_mobile = [
         unit_fn=lambda metric: UnitOfTemperature.CELSIUS
         if metric
         else UnitOfTemperature.FAHRENHEIT,
-        value_fn=lambda data, _: cast(float, data) if isinstance(data, (int | float)) else 0.0,
+        value_fn=lambda data, _: cast(float, data),
     ),
     WeatherSensorEntityDescription(
         key=FIELD_PRESSURE,
@@ -339,7 +308,7 @@ current_condition_sensor_descriptions_mobile = [
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.PRESSURE,
         unit_fn=lambda metric: UnitOfPressure.MBAR if metric else UnitOfPressure.INHG,
-        value_fn=lambda data, _: cast(float, data) if isinstance(data, (int | float)) else 0.0,
+        value_fn=lambda data, _: cast(float, data),
     ),
     WeatherSensorEntityDescription(
         key=FIELD_WINDGUST,
@@ -350,7 +319,7 @@ current_condition_sensor_descriptions_mobile = [
         unit_fn=lambda metric: UnitOfSpeed.KILOMETERS_PER_HOUR
         if metric
         else UnitOfSpeed.MILES_PER_HOUR,
-        value_fn=lambda data, _: cast(float, data) if isinstance(data, (int | float)) else 0.0,
+        value_fn=lambda data, _: cast(float, data),
     ),
     WeatherSensorEntityDescription(
         key=FIELD_WINDSPEED,
@@ -361,16 +330,15 @@ current_condition_sensor_descriptions_mobile = [
         unit_fn=lambda metric: UnitOfSpeed.KILOMETERS_PER_HOUR
         if metric
         else UnitOfSpeed.MILES_PER_HOUR,
-        value_fn=lambda data, _: cast(float, data) if isinstance(data, (int | float)) else 0.0,
+        value_fn=lambda data, _: cast(float, data),
     ),
     WeatherSensorEntityDescription(
         key="pressureTendencyTrend",
         name="Pressure Tendency Trend",
         icon="mdi:gauge",
-        value_fn=lambda data, _: cast(str, data) if isinstance(data, str) else "Unknown",
+        value_fn=lambda data, _: cast(str, data),
     ),
-    # Pollen is not available from mobile api
-    # WeatherSensorEntityDescription(
+    # WeatherSensorEntityDescription( # Pollen is not available from mobile api
     #     key="pollen_levels",
     #     name="Pollen Levels",
     #     icon="mdi:flower",
@@ -383,38 +351,38 @@ current_condition_sensor_descriptions_mobile = [
     #     value_fn=lambda data, _: cast(
     #         str, ". ".join(i.capitalize() for i in data.lstrip(" ")[0:254].split(". "))
     #     ),
-        # Pollen Type can be very long, so truncate to 254 characters; and capitalize each sentence
+        # Pollen Type can be very long, so truncate to 254 characters; and capitalise each sentence
     # ),
     WeatherSensorEntityDescription(
         key="drying_index_morning",
         name="Clothes Drying Time - Morning",
         icon="mdi:tshirt-crew",
-        value_fn=lambda data, _: cast(str, data.replace("Morning: ", "")) if isinstance(data, str) else "Unknown",
+        value_fn=lambda data, _: cast(str, data.replace("Morning: ", "")),
     ),
     WeatherSensorEntityDescription(
         key="drying_index_afternoon",
         name="Clothes Drying Time - Afternoon",
         icon="mdi:tshirt-crew",
-        value_fn=lambda data, _: cast(str, data.replace("Afternoon: ", "")) if isinstance(data, str) else "Unknown",
+        value_fn=lambda data, _: cast(str, data.replace("Afternoon: ", "")),
     ),
     WeatherSensorEntityDescription(
         key="weather_warnings",
         name="MetService Weather Warnings",
         icon="mdi:alert",
-        value_fn=lambda data, _: truncate_state(data),
-        attr_fn=lambda data: truncate_attribute(data),
+        value_fn=lambda data, _: (data[:250] + '...') if data and len(data) > 255 else (data or "No warnings"),
+        attr_fn=lambda data: {"warnings": data} if data else {},
     ),
     WeatherSensorEntityDescription(
         key="fire_season",
         name="Fire Season",
         icon="mdi:fire",
-        value_fn=lambda data, _: cast(str, data) if isinstance(data, str) else "Unknown",
+        value_fn=lambda data, _: cast(str, data),
     ),
     WeatherSensorEntityDescription(
         key="fire_danger",
         name="Fire Danger",
         icon="mdi:fire",
-        value_fn=lambda data, _: cast(str, data) if isinstance(data, str) else "Unknown",
+        value_fn=lambda data, _: cast(str, data),
     ),
     WeatherSensorEntityDescription(
         key="tides_high",
@@ -429,10 +397,12 @@ current_condition_sensor_descriptions_mobile = [
                 and dt_util.parse_datetime(a["time"]) is not None
                 and dt_util.parse_datetime(a["time"]) > dt_util.now(AUCKLAND_TIMEZONE)
             ][0]
-            if isinstance(data, list) and data else None
-        ),
+            if data
+            else None
+        )
+        if isinstance(data, list)
+        else None,
     ),
-
     WeatherSensorEntityDescription(
         key="tides_low",
         name="Next Low Tide",
@@ -447,7 +417,10 @@ current_condition_sensor_descriptions_mobile = [
                 and dt_util.parse_datetime(a["time"]) is not None
                 and dt_util.parse_datetime(a["time"]) > dt_util.now(AUCKLAND_TIMEZONE)
             ][0]
-            if isinstance(data, list) and data else None
-        ),
+            if data
+            else None
+        )
+        if isinstance(data, list)
+        else None,
     ),
 ]
